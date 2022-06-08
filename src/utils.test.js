69e41423cea9a742af1a93d5562b9ecfb94a6003
Finn Fitzsimons <fitzsimons.finn@gmail.com>
2022-06-08 23:06:28 +0300

    Dry up util test code

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,21 +1,23 @@
 import { getSmallestBreakpoint } from './utils';
 
+const BREAKPOINTS = { a: 30, b: 10, c: 20, d: 40 };
+
 describe('getSmallestBreakpoint', () => {
   test('default', () => {
     expect(
-      getSmallestBreakpoint(15, { a: 30, b: 10, c: 20, d: 40 })
+      getSmallestBreakpoint(15, BREAKPOINTS)
     )
     .toBe('c');
   });
   test('bigger than max', () => {
     expect(
-      getSmallestBreakpoint(50, { a: 30, b: 10, c: 20, d: 40 })
+      getSmallestBreakpoint(50, BREAKPOINTS)
     )
     .toBe('d');
   });
   test('less than min', () => {
     expect(
-      getSmallestBreakpoint(0, { a: 30, b: 10, c: 20, d: 40 })
+      getSmallestBreakpoint(0, BREAKPOINTS)
     )
     .toBe('b');
   });
